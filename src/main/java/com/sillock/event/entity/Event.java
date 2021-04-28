1b822b4b9531dba730a7a9fa7be5240e753b22d4
catnap421 <wlq7410@naver.com>
2021-04-28 12:03:36 +0900

    Add: 도메인 별 Entity 추가

---
+++ src/main/java/com/sillock/event/entity/Event.java
@@ -0,0 +1,12 @@
+package com.sillock.event.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Event {
+}
---
+++ src/main/java/com/sillock/member/entity/Member.java
@@ -0,0 +1,12 @@
+package com.sillock.member.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Member {
+}
