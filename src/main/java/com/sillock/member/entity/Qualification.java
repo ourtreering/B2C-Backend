fd109c4e8f9ae65263ddaf85d4748abfdb15d43f
shinequasar <shinequasar@gmail.com>
2021-04-30 11:12:23 +0900

    Add:Qualification.java 엔티티 매핑

--- src/main/java/com/sillock/member/entity/Qualification.java
+++ src/main/java/com/sillock/member/entity/Qualification.java
@@ -3,10 +3,35 @@ package com.sillock.member.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @NoArgsConstructor
 @Getter
 @Entity
 public class Qualification {
+    @Id
+    @Column(name="qualification_id")
+    private Long qualificationId;
+
+    @Column(name="member_id")
+    private Long memberId;
+
+    @Column(name = "content", length = 255)
+    private String content;
+
+    @Column(name = "is_verified", nullable = true)
+    private Boolean isVerified;
+
+    @Column(name = "reg_date")
+    private String regDate;
+
+    @Column(name = "mod_date")
+    private String modDate;
+
+    @Column(name = "due_date")
+    private String dueDate;
+
 }
