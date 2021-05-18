e9a94d7aa52c10e4b295993a250880f9c5d0ed38
shinequasar <shinequasar@gmail.com>
2021-05-19 05:19:41 +0900

    Add:WebSecurityConfig 반영 및 수정

--- src/main/java/com/sillock/core/config/WebSecurityConfig.java
+++ src/main/java/com/sillock/core/config/WebSecurityConfig.java
@@ -1,20 +1,51 @@
 package com.sillock.core.config;
 
-import com.sillock.core.jwt.JwtUserDtailsService;
+
+import com.sillock.core.jwt.JwtAccessDiniedHandler;
+import com.sillock.core.jwt.JwtAuthenticationEntryPoint;
+import com.sillock.member.jwt.JwtTokenProvider;
+import com.sillock.member.jwt.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Autowired
-    private JwtUserDtailsService jwtUserDtailsService;
+    private JwtTokenProvider jwtTokenProvider;
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private JwtAccessDiniedHandler jwtAccessDiniedHandler;
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(jwtUserDtailsService);
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                //401, 403에러 검사
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDiniedHandler)
+
+                .and()
+                .headers().frameOptions().disable()
+
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/","/css/**","images/**","/img/**","/lib/*8",
+                            "/js/**","/favicon.ico","/api/signup").permitAll()
+                    .antMatchers("/user/**").hasRole(Role.USER.name())
+                    .antMatchers("/admin/**").hasRole(Role.ADMIN.name())
+                    .anyRequest().authenticated()
+
+                //JwtSecurityConfig를 통해 jwtRequestFilter 등록
+                .and()
+                .apply(new JwtSecurityConfig(jwtTokenProvider));
+
     }
+
 }
