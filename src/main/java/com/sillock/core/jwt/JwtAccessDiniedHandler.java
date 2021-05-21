ebad06dfc15f63c1497bd5697263179786c043c5
shinequasar <shinequasar@gmail.com>
2021-05-21 13:11:42 +0900

    Chore:junit 추가

--- src/main/java/com/sillock/core/jwt/JwtAccessDiniedHandler.java
+++ src/main/java/com/sillock/core/jwt/JwtAccessDiniedHandler.java
@@ -14,6 +14,6 @@ public class JwtAccessDiniedHandler implements AccessDeniedHandler {
     //403 Forbidden 에러
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        response.sendError(HttpServletResponse.SC_FORBIDDEN,"403 Forbidden");
     }
 }
--- src/main/java/com/sillock/core/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/sillock/core/jwt/JwtAuthenticationEntryPoint.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,7 +17,4 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          AuthenticationException authException) throws IOException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"401 권한없음.");
     }
-
-    //403 Forbidden 에러
-
 }
