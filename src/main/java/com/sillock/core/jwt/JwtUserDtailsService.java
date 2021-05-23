5db36168350830717ecf3bde89bbed857793d41a
shinequasar <shinequasar@gmail.com>
2021-05-23 23:56:27 +0900

    Refactor:private key getter로 접근가능하게하기

--- src/main/java/com/sillock/core/jwt/JwtUserDtailsService.java
+++ src/main/java/com/sillock/core/jwt/JwtUserDtailsService.java
@@ -1,36 +1,32 @@
-package com.sillock.core.jwt;
-
-import com.sillock.member.entity.Member;
-import com.sillock.member.jwt.Role;
-import com.sillock.member.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.HashSet;
-import java.util.Set;
-
-@Service
-public class JwtUserDtailsService implements UserDetailsService {
-
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    //들어온 email로 Member를 찾아 User객체로 반환
-    //UniqueCode -> password로 수정해야함
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Member member = memberRepository.findByEmail(email).orElseThrow(()->new UsernameNotFoundException(email));
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        grantedAuthorities.add(new SimpleGrantedAuthority(Role.USER.getValue()));
-        return new User(member.getEmail(), member.getUniqueCode(),grantedAuthorities);
-    }
-
-
-}
+//package com.sillock.core.jwt;
+//
+//import com.sillock.member.entity.Member;
+//import com.sillock.core.jwt.Role;
+//import com.sillock.member.repository.MemberRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class JwtUserDtailsService implements UserDetailsService {
+//
+//    @Autowired
+//    private MemberRepository memberRepository;
+//
+//    //들어온 email로 Member를 찾아 User객체로 반환
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//         JwtUserDtails member = memberRepository.findByEmail(email)
+//                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+//        return new member;
+//    }
+//
+//
+//}
---
+++ src/main/java/com/sillock/web/AuthController.java
@@ -0,0 +1,4 @@
+package com.sillock.web;
+
+public class AuthController {
+}
