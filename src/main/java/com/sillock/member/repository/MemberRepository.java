2430e6f60b2235209072b95eb77052aaaef78d39
shinequasar <shinequasar@gmail.com>
2021-05-26 00:48:47 +0900

    Fix:build 이슈 수정

--- src/main/java/com/sillock/member/repository/MemberRepository.java
+++ src/main/java/com/sillock/member/repository/MemberRepository.java
@@ -10,7 +10,4 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByEmail(String email);
     Optional<Member> findByUniqueCode(String uniqueCode);
-    Optional<Member> findByUidAndProcider(String uid, String provider);
-//    @EntityGraph(attributePaths = "authorities")
-//    Optional<Member> findOneWithAuthoritiesByname(String name);
 }
