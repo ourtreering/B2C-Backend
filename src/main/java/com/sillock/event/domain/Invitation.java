28c2031111c93cac12e3ebb4e86d1d43c3deaf0c
shinequasar <shinequasar@gmail.com>
2021-06-03 16:24:15 +0900

    Add: invitation 관련 엔티티 추가

--- src/main/java/com/sillock/event/domain/Invitation.java
+++ src/main/java/com/sillock/event/domain/Invitation.java
@@ -1,8 +1,19 @@
 package com.sillock.event.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Invitation {
+    @EmbeddedId
+    private InvitationPK Id;
+
     private Boolean isApproved;
     private LocalDateTime regDate;
 }
---
+++ src/main/java/com/sillock/event/domain/InvitationPK.java
@@ -0,0 +1,27 @@
+package com.sillock.event.domain;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+@NoArgsConstructor
+public class InvitationPK implements Serializable {
+    private Long memberId;
+    private Long closedEventId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof InvitationPK)) return false;
+        InvitationPK that = (InvitationPK) o;
+        return memberId.equals(that.memberId) && closedEventId.equals(that.closedEventId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(memberId, closedEventId);
+    }
+}
