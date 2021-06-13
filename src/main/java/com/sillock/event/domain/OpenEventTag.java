13e5ef28ef847a221eb362b6dfdd6ce5186f1e34
shinequasar <shinequasar@gmail.com>
2021-06-13 22:33:00 +0900

    Add:openEventTag 매핑

--- src/main/java/com/sillock/event/domain/OpenEventTag.java
+++ src/main/java/com/sillock/event/domain/OpenEventTag.java
@@ -1,5 +1,27 @@
 package com.sillock.event.domain;
 
-public class OpenEventTag {
+import com.sillock.tag.domain.Tag;
+import lombok.*;
 
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class OpenEventTag {
+    @EmbeddedId
+    private OpenEventTagPK id;
+
+    @MapsId("tagId")
+    @OneToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @MapsId("openEventId")
+    @OneToOne
+    @JoinColumn(name = "open_event_id")
+    private OpenEvent openEvent;
 }
