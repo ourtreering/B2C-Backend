f3c7842222ce057f72e501112950d478185a7803
catnap421 <wlq7410@naver.com>
2021-07-12 15:39:12 +0900

    Add: sillog service register 기능 추가

--- src/main/java/com/sillock/domain/sillog/service/SillogService.java
+++ src/main/java/com/sillock/domain/sillog/service/SillogService.java
@@ -2,22 +2,34 @@ package com.sillock.domain.sillog.service;
 
 import com.sillock.domain.sillog.model.entity.Qna;
 import com.sillock.domain.sillog.model.entity.Sillog;
+import com.sillock.domain.sillog.repository.QnaRepository;
 import com.sillock.domain.sillog.repository.SillogRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class SillogService {
     private final SillogRepository sillogRepository;
+    private final QnaRepository qnaRepository;
 
+    @Transactional(readOnly = true)
     public List<Sillog> getSillogList(Long memberId){
 
         return sillogRepository.findAllById(memberId);
+    }
+
+    @Transactional
+    public void register(Sillog sillog){
+        qnaRepository.saveAll(sillog.getQnaData());
+        sillogRepository.save(sillog);
     }
 }
